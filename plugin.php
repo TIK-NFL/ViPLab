9c081dd10e70df5f03813f88fe5b5e15908d8f7e
smeyer <smeyer.ilias@gmx.de>
2015-10-22 17:15:00 +0200

    Added 5.1 compatability. no code changes.

--- plugin.php
+++ plugin.php
@@ -4,12 +4,12 @@
 $id = "assviplab";
  
 // code version; must be changed for all code changes
-$version = "0.2.01";
+$version = "5.1.01";
  
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "5.0.3";
-$ilias_max_version = "5.0.999";
+$ilias_min_version = "5.1.3";
+$ilias_max_version = "5.1.999";
 
 // add the responsible person for the plugin
 $responsible = "Stefan Meyer"; 
