7249f69cb3ea1c6aa301eec181ba535d73f540dc
Per Pascal Seeland <pascal.seeland@tik.uni-stuttgart.de>
2021-09-02 09:55:21 +0200

    Update compatibiltiy to ILIAS 7

--- plugin.php
+++ plugin.php
@@ -9,7 +9,7 @@ $version = "5.4.1";
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "5.4.0";
-$ilias_max_version = "5.4.999";
+$ilias_max_version = "7.999";
 
 // add the responsible person for the plugin
 $responsible = "Per Pascal Seeland"; 
